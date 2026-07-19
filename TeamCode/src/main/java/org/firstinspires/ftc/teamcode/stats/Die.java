19372f4111c8984c7c5d8185b7311a874d28a2a0
J-f12 <j3ss1ca.fu.1024@gmail.com>
2026-07-19 19:06:45 -0400

    Die

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/stats/Die.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.stats;
+
+public class Die {
+    public Die(int numSides) {
+
+    }
+
+    public int roll() {
+        return 0;
+    }
+
+    public int sides() {
+        return 0;
+    }
+
+}
