c5db595031f9684d72df0e78d83b4799900870c5
Paul Blankenbaker <paul.blankenbaker@gmail.com>
2026-08-02 19:07:38 -0400

    MVP "Car Drive" mode for Chemung fair

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotTeleopTankDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotTeleopTankDrive.java
@@ -1,70 +1,24 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-/*
- * This OpMode executes a Tank Drive control TeleOp a direct drive robot
- * The code is structured as an Iterative OpMode
- *
- * In this mode, the left and right joysticks control the left and right motors respectively.
- * Pushing a joystick forward will make the attached motor drive forward.
- * It raises and lowers the claw using the Gamepad Y and A buttons respectively.
- * It also opens and closes the claws slowly using the left and right Bumper buttons.
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
- */
 
-@TeleOp(name="Tank Drive", group="Robot")
-public class RobotTeleopTankDrive extends LinearOpMode {
+@TeleOp(name="Car Drive", group="Robot")
+public class RobotTeleopTankDrive extends OpMode {
     /* Declare OpMode members. */
     public DcMotor  backLeftDrive   = null;
     public DcMotor  backRightDrive   = null;
     public DcMotor  frontLeftDrive   = null;
     public DcMotor  frontRightDrive  = null;
 
-    @Override
-    public void runOpMode() throws InterruptedException {
-
-    }
-
     /*
      * Code to run ONCE when the driver hits INIT
      */
-    //@Override
-    public void initTODO() {
+    @Override
+    public void init() {
         // Define and Initialize Motors
         // Control hub 0 & 1
         backLeftDrive  = hardwareMap.get(DcMotor.class, "bl");
@@ -73,28 +27,18 @@ public class RobotTeleopTankDrive extends LinearOpMode {
         backRightDrive = hardwareMap.get(DcMotor.class, "br");
         frontRightDrive = hardwareMap.get(DcMotor.class, "fr");
 
-        // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
-        // Pushing the left and right sticks forward MUST make robot go forward. So adjust these two lines based on your first test drive.
-        // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
         backRightDrive.setDirection(DcMotor.Direction.REVERSE);
         frontRightDrive.setDirection(DcMotor.Direction.REVERSE);
         backLeftDrive.setDirection(DcMotor.Direction.FORWARD);
         frontLeftDrive.setDirection(DcMotor.Direction.FORWARD);
 
-        // If there are encoders connected, switch to RUN_USING_ENCODER mode for greater accuracy
-        // leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        // rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        // Send telemetry message to signify robot waiting;
         telemetry.addData(">", "Robot Ready.  Press START.");    //
     }
 
 
-    /*
-     * Code to run REPEATEDLY after the driver hits START but before they hit STOP
-     */
-    //@Override
-    public void loopTODO() {
+    @Override
+    public void loop() {
         double rotation;
         double fowardBackward;
 
@@ -104,11 +48,18 @@ public class RobotTeleopTankDrive extends LinearOpMode {
         fowardBackward = -gamepad1.left_stick_y;
         rotation = gamepad1.right_stick_x;
 
-        if (Math.abs(fowardBackward) > 0.05) {
-            setTankDrivePower(fowardBackward, fowardBackward);
-        } else {
-            setTankDrivePower(rotation, -rotation);
-        }
+        double leftPower = fowardBackward + rotation;
+        double rightPower = fowardBackward - rotation;
+
+       double mag = Math.max(Math.abs(leftPower), Math.abs(rightPower));
+       if (mag > 1.0)
+       {
+           leftPower /= mag;
+           rightPower /= mag;
+       }
+
+
+        setTankDrivePower(leftPower, rightPower);
     }
 
     private void setTankDrivePower(double leftPower, double rightPower) {
